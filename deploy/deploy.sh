486ac3e0ca5bfcb1348e9460ce6fac54e975b905
Mojumder, Santanu <Santanu.Mojumder@landsend.com>
2021-03-27 14:31:59 -0500

    first version

---
+++ deploy/deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+function build_container() {
+  #gcr.io/bootiful/bk
+  APP_NAME=bk
+  GCR_IMAGE_NAME=gcr.io/bootiful/${APP_NAME}
+  docker images -q $GCR_IMAGE_NAME | while read l; do docker rmi -f $l; done
+
+  echo "Building $GCR_IMAGE_NAME"
+  mvn -f pom.xml \
+    -DskipTests=true \
+    clean spring-boot:build-image -e \
+    -Dspring.profiles.active=production,cloud \
+    -Dspring-boot.build-image.imageName=$GCR_IMAGE_NAME \
+    clean spring-boot:build-image -e
+  IMAGE_ID="$(docker images -q $GCR_IMAGE_NAME)"
+  docker tag "${IMAGE_ID}" ${GCR_IMAGE_NAME}:latest
+  docker push ${GCR_IMAGE_NAME}:latest
+}
+
+echo "going to deploy the application"
+NS=bk
+kubectl get ns/$NS || kubectl create namespace $NS
+cd $(dirname $0)/..
+#build_container
+cd $(dirname $0)
+kubectl apply -f bk.yaml -n $NS
